79691baa5a516c41529c1112671c8a9cd5d21eb8
Ryan Short <shorttryan@gmail.com>
2022-09-17 22:14:32 -0400

    adds card images and links them to card values

--- main.js
+++ main.js
@@ -28,10 +28,10 @@ The game ends when one player has won all the cards.
  
  class Game {
 	 constructor(){
-		 this.players = [];
+		 this.players = []; //every game you start will have this array including your new players.
 	 };
 	 
-	 start(playerOneName, playerTwoName){
+	 start(playerOneName, playerTwoName){ //creating a new deck from the deck class & shuffling it.
 		 this.players.push(new Player(playerOneName));
 		 this.players.push(new Player(playerTwoName));
 		 let newDeck = new Deck();
@@ -40,12 +40,12 @@ The game ends when one player has won all the cards.
 		 return [this.players, shuffDeck];
 	 };
 	 
-	 deal(players, deck){
-		 for(var i = 0; i < deck.length; i = i+2){
-			 this.players[0].playerCards.push(deck[i]);
-			 this.players[1].playerCards.push(deck[i+1]);
+	 deal(players, deck){ //distributes prev shuffled cards to each player, alternating between cards when dealing.
+		 for(var i = 0; i < deck.length; i = i+2){ // card from deck = position i. every second card will handed out.
+			 this.players[0].playerCards.push(deck[i]); //this player gets every second card starting from the FIRST(i=0) position.
+			 this.players[1].playerCards.push(deck[i+1]);//this player gets every second card starting from the SECOND(i=1) position.
 		 };
-		 return [players[0], players[1]];
+		 return [players[0], players[1]]; //returns each player's hand.
 	 };
 	 
 	 
@@ -54,11 +54,11 @@ The game ends when one player has won all the cards.
 		 const numOfCards = cards.length;
 		 console.log(numOfCards, 'number of cards during compare');
 		 var p1val, p2val;
-		 if (numOfCards === 2){
+		 if (numOfCards === 2){ //checks the total # of cards played first (in a normal hand).
 			 console.log('if 2 is num of cards');
 			 console.log(cards[0]['value']);
-			 p1val = cards[0]['value'];
-			 p2val = cards[1]['value'];
+			 p1val = cards[0]['value']; //if you played a normal hand, stores the card value (ie. ace of clubs = val of 14) that the player played.
+			 p2val = cards[1]['value'];  // ""player 2's card
 		 }else{
 			 console.log('if more than two cards');
 			 p1val = cards[3]['value'];
@@ -67,89 +67,36 @@ The game ends when one player has won all the cards.
 
 		 //p1val = 5;
 		 //p2val = 5;
-		 console.log(p1val, p2val);
+		 console.log(p1val, p2val); // if statements of comparing the players' cards' values.
 		 if(p1val > p2val){
 			userInstruction.innerHTML = players[0].playerName + ' has won the hand.';
-			players[0].takeCards(players[0], cards);
+			players[0].takeCards(players[0], cards); //if you won, you're players[0], and you take the cards played into your hand.
 			warCheck = false;
 		 }else if (p2val > p1val){
 			userInstruction.innerHTML = players[1].playerName + ' has won the hand.'
-			players[1].takeCards(players[1], cards);
+			players[1].takeCards(players[1], cards); //does the same thing for player two if they win.
 			warCheck = false;
-		 }else if (p1val === p1val){
-			userInstruction.innerHTML = 'Players have tied, this means WAR!';
+		 }else if (p1val === p1val){ //if they had the same value, then you go to WAR.
+			userInstruction.innerHTML = 'Players have tied, this means WAR! Click DRAW to play WAR!';
+			console.log(oldCards, 'check old cards when players have tied, should be length 0 here');
 			console.log(cards[0], cards[1]);
-			warCheck = true;
-			newGame.war();
-			oldCards = cards;
+			warCheck = true; //set to true bc now we're at WAR.
+			for (var i = 0;i<cards.length;i++){ //put the 2 faceup played cards into the 'stored cards(oldCards)' array for later distribution, depending on the victor of the WAR.
+				oldCards.push(cards[i]);
+			};
+			console.log(oldCards, 'check old cards when players have tied, should be length 2 here');
 		 };
+		 //check if a player has won after the hand is over
+		 newGame.updateDeck(players);
+		 newGame.checkWin(players);
 	 };
 	 
-	 war(){
-		 console.log('war has been called');
-		 setTimeout(function(){
-			userInstruction.innerHTML = 'Click DRAW to play WAR!';
-		 },2500);
-		
-/* 		 var newCards = [];
-		 var saveHand = [];
-		 const numOfCards = cards.length;
-		 if (numOfCards === 2){
-			const p1val = cards[0]['value'];
-			const p2val = cards[1]['value'];
-		 }else{
-			 const p1val = cards[1]['value'];
-			 const p2val = cards[3]['value'];
-		 }; */
-		 //const p1val = 5;
-		 //const p2val = 5;
-		/*  console.log(p1val, p2val);
-		 var p1pos = 1;
-		 var p2pos = 3;
-		 while (p2val === p1val){
-			console.log('while loop');
-			 for (var i = 0; i < players.length; i++){
-				 console.log('entered for loop');
-				 console.log(players.length, 'player array length');
-				 newCards=players[i].playHandForWar(players[i]);
-				 console.log(newCards[0]['rank'], 'rank');
-				 for (var j=0; j< newCards.length; j++){
-					 saveHand.push(newCards[j]);
-				 };
-				 console.log(saveHand, 'saveHand array');
-				 console.log(p1pos,'p1pos');
-				 console.log(i,'i');
-				 if(i===0){
-					 warOutput[i].innerHTML = 'FACE DOWN CARD; ' + saveHand[p1pos]['rank'] + ' of ' + saveHand[p1pos]['suit'];
-				 } else if (i===1){
-					 warOutput[i].innerHTML = 'FACE DOWN CARD; ' + saveHand[p2pos]['rank'] + ' of ' + saveHand[p2pos]['suit'];
-				 };
-			 };
-			 console.log(p1pos, 'player 1 pos');
-			 console.log(saveHand[1]['value'], 'save hand 1 value');
-			 var p1FaceUpVal = saveHand[p1pos]['value'];
-			 var p2FaceUpVal = saveHand[p2pos]['value'];
-
-			 if(p1FaceUpVal > p2FaceUpVal){
-				userInstruction.innerHTML = players[0].playerName + ' has won the hand.';
-				saveHand.push(cards);
-				players[0].takeCards(players[0], saveHand);
-				break;
-			}else if (p1FaceUpVal <  p2FaceUpVal){
-				userInstruction.innerHTML = players[1].playerName + ' has won the hand.';
-				saveHand.push(cards);
-				players[1].takeCards(players[1], saveHand);
-				break;
-			}else if (p1FaceUpVal === p2FaceUpVal){
-				userInstruction.innerHTML = 'Players have tied, continue WAR!';
-				p1val = p1FaceUpVal;
-				p2val = p2FaceUpVal
-			};
-			p1pos = p1pos+4;
-			p2pos = p2pos+4;
-		 }; */
-		 
+	 updateDeck(players){ //supposed to update the # of cards in hand
+		 for(var i = 0; i < deckCount; i++){
+			 deckCount[i].innerHTML = '('+ players[i].playerCards.length + ')';
+		 };
 	 };
+
 	 
 	 checkWin(players){
 		 console.log('check win has been called');
@@ -193,18 +140,18 @@ The game ends when one player has won all the cards.
 		 return [faceDownCard1,faceDownCard2,faceDownCard3, faceUpCard];
 	 };
 	 
-	 takeCards(player, cards){
+	 takeCards(player, cards){ //the player who won receives the array of cards that were played.
 		 console.log('take cards has been called');
-		 for(var i = 0; i < cards.length; i++){
+		 for(var i = 0; i < cards.length; i++){ //loops thru whole played-cards array
 			 console.log(cards[i], 'take card');
-			 player.playerCards.unshift(cards[i]);
+			 player.playerCards.unshift(cards[i]); // taking any received cards from winning, and placing them at the BOTTOM of their hand/deck.
 		 };
-		 setTimeout(function(){
+		 setTimeout(function(){ // 3 seconds after a hand is won, it updates the instructions to the user and clears the outputs for the next round of play.
 			userInstruction.innerHTML = 'Click DRAW to play the next hand.';
 			cardOutput[0].innerText = '';
 			cardOutput[1].innerText = '';
-			//warOutput[0].innerText = '';
-			//warOutput[1].innerText = '';
+			//warOutput[0].innerHTML = '';
+			//warOutput[1].innerHTML = '';
 		 },3000); 
 	 };
 	 
@@ -215,9 +162,9 @@ The game ends when one player has won all the cards.
 		 this.cards=[];
 	 };
 	 
-	 createDeck(){
+	 createDeck(){ //all card values for the 52 total cards.
 		 let suits = ['Clubs','Diamonds','Hearts','Spades'];
-		 let ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King','Ace'];
+		 var ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King','Ace'];
 		 let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,14];
 		 
 		 for (var i = 0; i < suits.length; i++){
@@ -225,7 +172,7 @@ The game ends when one player has won all the cards.
 				 this.cards.push(new Card(suits[i], ranks[j], values[j]));
 			 };
 		 };
-		 return this.cards;
+		 return this.cards; //makes the initial deck of cards
 	 };
 	 
 	 shuffleDeck(arr){	//base shuffle off of Knuth (fisher-yates) shuffle
@@ -274,12 +221,12 @@ for (var j = 0; j < ply1cards.length; j++){
 	console.log(ply1cards[j]);
 }; */
 
-const newGame = new Game();
-const startGame = newGame.start('Ryan','Challenger');
+const newGame = new Game(); //creates the Game object using the constructor inside the Game class.
+const startGame = newGame.start('Ryan','Challenger'); //starts the game, returns 2 separate players and their hands
 var players = newGame.deal(startGame[0],startGame[1]);
-var currentCards = [];
-var oldCards = [];
-var warCheck = false;
+var currentCards = []; //blank array of currently 'played' cards to compare values.
+var oldCards = []; //the placevalue holder of current values of the 'tied' cards that send you to war, so more cards can be used for the war.
+var warCheck = false; //we're not at war by default
 //var playerOne = dealGame[0];
 //var playerTwo = dealGame[1];
 //var playerOneName = playerOne.playerName;
@@ -297,61 +244,98 @@ var warCheck = false;
 
 const drawButton = document.querySelector('[data-draw]');
 const cardOutput = document.querySelectorAll('.card');
-const warOutput = document.querySelectorAll('.war');
-const userInstruction = document.querySelector('[data-instruction]');
-const p1DeckImage = document.querySelector('[data-p1-deck]');
-const p2DeckImage = document.querySelector('[data-p2-deck]');
+//const warOutput = document.querySelectorAll('.war'); // will only output 3 facedown cards and 1 face up in the event of a 'War'.
+const deckCount = document.querySelectorAll('.deckCount');//to update total number of cards each player has in deck, currently not working
+const userInstruction = document.querySelector('[data-instruction]'); //top header line, displaying updated instructions/announcements to the user.
+//const p1DeckImage = document.querySelector('[data-p1-deck]');
+//const p2DeckImage = document.querySelector('[data-p2-deck]');
 
 drawButton.addEventListener('click', button => {
-	//function to remove a card from the player's deck and display data for the card that was removed
+	//for some reason player 2 html elements disappear clicking draw button. cannot figure out why
 	console.log('draw button clicked');
+	console.log(oldCards, 'check old cards at beginning of draw');
 	console.log(warCheck);
-	if (!warCheck){
-		for (var i = 0; i < cardOutput.length; i++){
+	if (!warCheck){ //if war is false (no triggered war event) then it's a normal round and this for loop WILL run.
+		console.log('enter draw if statement');
+		for (var i = 0; i < 2; i++){ //
 			//console.log(dealGame[i]);
 			//console.log(dealGame[i].playerCards);
+			console.log('enter for loop for if war is false');
 			var hand = players[i].playHand(players[i].playerCards);
-			//console.log(hand['suit']);
-			cardOutput[i].innerText = hand['rank'] + ' of ' + hand['suit'];
-			currentCards[i] = hand;
-		};
-	} else{
+			console.log(hand['rank']);
+			console.log(hand['suit']);
+			
+			
+			console.log("images/" + hand['rank'] + hand['suit'] + ".png");
+			if(i===0){
+				var img = document.createElement('img');
+				img.src = ("images/" + hand['rank'] + hand['suit'] + ".png");
+				var imgElement1 = document.getElementById('card1');
+				imgElement1.appendChild(img);
+			}else{
+				var img2 = document.createElement('img');
+				img2.src = ("images/" + hand['rank'] + hand['suit'] + ".png");
+				var imgElement2 = document.getElementById('card2');
+				imgElement2.appendChild(img2);
+			};
+			
+			//img2.src = ("images/" + cards[1]['ranks'] + cards[1]['suits'] + ".png");
+			// cardOutput[i].innerText = hand['rank'] + ' of ' + hand['suit']; //that hand variable is calling the play-hand function of the player class.
+			currentCards[i] = hand; // shows the face-up diplayed card that was played.
+		}; // loops through each player, has each player draw a card, and then outputs the value to the html interface, then stores the card that they drew into the current cards array.
+		//currentCards = [{'suit': 'Hearts', 'rank': '8', 'value': 8},{'suit': 'Spades', 'rank': '8', 'value': 8}];
+		//console.log('reset current cards to for war',8,8);
+	} else{ // IF WE GO TO WAR...
 		console.log('draw for war');
-		for (var k = 0; k <warOutput.length; k++){
+		for (var k = 0; k <2; k++){ //for loop, looping through the amount of outputs we have (which is equal to the amount of players(2)).
 			console.log('enter draw for war for loop');
 			console.log(k, 'k for war draw for loop');
 			console.log(players[k]);
-			var hand = players[k].playHandForWar(players[k]);
-			console.log(hand, 'hand for war');
-			warOutput[k].innerText = '3 FACE DOWN CARDS; ' + hand[3]['rank'] + ' of ' + hand[3]['suit'];
-			if (k === 0){
+			var hand = players[k].playHandForWar(players[k]); //takes the whole player, breaks out the deck inside the function, then deals 4 cards.
+			console.log(hand, 'hand for war'); // the hand will be an array of 4 cards. 3 face down, 1 face up.
+			cardOutput[k].innerHTML = cardOutput[k].innerHTML + '; 3 FACE DOWN CARDS; ' + hand[3]['rank'] + ' of ' + hand[3]['suit']; // text div that outputs the WAR results.
+			console.log('set war output',k);
+			if (k === 0){ //player1 will have an array of played hand [0-4]. passing those played cards into an array of values 0-7 depending on each player.
+                            //the below hand value is representing each card's position within each player's current played hand in the WAR.
 				console.log('if k is 0');
 				currentCards[0] = hand[0];
 				currentCards[1] = hand[1];
 				currentCards[2] = hand[2];
 				currentCards[3] = hand[3];
-			}else if (k===1){
+				console.log(currentCards, 'check current cards if first player has drawn war');
+				console.log(oldCards, 'check old cards if first player has drawn war');
+			}else if (k===1){ // player two's cards are in the 8-card array as 4-7, and player1 as 0-3.
 				console.log('if k is 1');
 				currentCards[4] = hand[0];
 				currentCards[5] = hand[1];
 				currentCards[6] = hand[2];
 				currentCards[7] = hand[3];
+				console.log(currentCards, 'check current cards if second player has drawn war');
+				console.log(oldCards, 'check old cards if second player has drawn war');
 			};
+			console.log('exit loop for war from draw button');
 		};
-		if(oldCards.length !== 0){
+		
+		console.log('exit if statement for draw button');
+		console.log(oldCards,'check old cards array after draw if statment');
+		if(oldCards.length !== 0){ //if you've gone to war, it will store the tied prev played cards, and move them into the 'take-hands' function.
+			console.log('old cards if statement');
 			for(var n = 0; n<oldCards.length; n++){
-				currentCards.push(oldCards[n]);
+				currentCards.push(oldCards[n]); // takes the original 'tied' cards (that triggered the WAR), and places them into the total played cards array so they can be included in the 'taken-cards' when someone wins.
 			};
+			oldCards = [];
 		};
 	};
-	setTimeout(function(){
-		newGame.compareHand(currentCards, players);
+
+	setTimeout(function(){ //gives you 3 seconds of screen time to see hand, THEN runs the compareHand function to determine results of said hand.
+		console.log('try to call compare hand');
+		newGame.compareHand(currentCards, players); 
 	},3000);
 	
 	//checkWin(players)
-	setTimeout(function(){
-		newGame.checkWin(players);
-	},8000);
+	//setTimeout(function(){
+		//newGame.checkWin(players);
+	//},8000);
 	
 });
 
